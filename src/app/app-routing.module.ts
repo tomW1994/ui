add344e81cf39471321e199c1dc1fc724e7a5c3e
unknown <tomweitze@gmx.de>
2021-05-11 16:45:14 +0200

    remove invalid imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StartComponent } from './start/start.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { JobAdmissionComponent } from './job-admission/job-admission.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { CommissioningComponent } from './commissioning/commissioning.component';
 
 const routes: Routes = [
-  { path: 'start-component', component: StartComponent },
-{ path: 'about-us-component', component: AboutUsComponent },
 { path: '', component: CommissioningComponent, pathMatch:'full' },
 { path: 'administration-component', component: AdministrationComponent },
 { path: 'job-admission-component', component: JobAdmissionComponent },];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StartComponent } from './start/start.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { CommissioningComponent } from './commissioning/commissioning.component';
 import { AdministrationComponent } from './administration/administration.component';
 import { JobAdmissionComponent } from './job-admission/job-admission.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterViewComponent } from './footer-view/footer-view.component';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,12 +16,9 @@ const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent,
-    AboutUsComponent,
     CommissioningComponent,
     AdministrationComponent,
     JobAdmissionComponent,
-    HeaderComponent,
     FooterViewComponent
   ],
   imports: [
